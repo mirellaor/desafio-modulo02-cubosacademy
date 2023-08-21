5b52300317960098b9af83c2ec6d11b0eb4179a2
mirellaor <mirellaor>
2023-08-21 01:14:04 -0300

    rota pra criar a conta

--- src/rotas.js
+++ src/rotas.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const contabancaria = require('./controladores/contabancaria');
 
 const rotas = express();
 
+rotas.post('/contabancaria', contabancaria.criarConta);
 
 module.exports = rotas;
