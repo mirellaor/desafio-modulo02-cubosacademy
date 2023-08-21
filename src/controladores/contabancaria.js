917f27e5f1791d91b1d08098d23e79f52d1db697
mirellaor <mirellaor>
2023-08-21 11:09:41 -0300

    função mais limpa

--- src/controladores/contabancaria.js
+++ src/controladores/contabancaria.js
@@ -3,19 +3,9 @@ let numero = 1;
 
 function criarConta(req, res) {
     const novaConta = { numero, ...req.body };
-
-    const { nome, cpf, data_nascimento, telefone, email, senha } = novaConta;
-    if (!nome || !cpf || !data_nascimento || !telefone || !email || !senha) {
-        res.status(400).json({ mensagem: "Não foi possível criar uma nova conta, pois algum campo não foi preenchido." });
-    } else if (contas.find((conta) => conta.cpf === cpf)) {
-        return res.status(409).json({ mensagem: "Já existe uma conta com esse CPF." })
-    } else if (contas.find((conta) => conta.email === email)) {
-        return res.status(409).json({ mensagem: "Já existe uma conta com esse email." })
-    } else {
-        contas.push(novaConta);
-        res.status(201);
-        numero++;
-    }
+    contas.push(novaConta);
+    res.status(201);
+    numero++;
 }
 
 module.exports = {
