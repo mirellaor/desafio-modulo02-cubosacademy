86b0f53bb7078a8cff74986d78779de43f890fcb
mirellaor <mirellaor>
2023-08-21 11:09:13 -0300

    adicionei o intermediador na rota de criar conta

--- src/rotas.js
+++ src/rotas.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const contabancaria = require('./controladores/contabancaria');
+const { verificacaoNovaConta } = require('./intermediadores');
 
 const rotas = express();
 
-rotas.post('/contabancaria', contabancaria.criarConta);
+rotas.post('/contabancaria', verificacaoNovaConta, contabancaria.criarConta);
 
 module.exports = rotas;
