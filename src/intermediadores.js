619fd5f8dab71876d84f5a96f724cd51f26f621f
mirellaor <mirellaor>
2023-08-21 18:21:09 -0300

    intermediador para atualizar os dados daa conta. Aproveitei pra reorganizar

--- src/intermediadores.js
+++ src/intermediadores.js
@@ -1,13 +1,20 @@
 const { banco, contas, saques, depositos, transferencias } = require('./bancodedados');
 
-function verificacaoNovaConta(req, res, next) {
+function verificaPreenchimentoCampos(req, res, next) {
     const { nome, cpf, data_nascimento, telefone, email, senha } = req.body;
     if (!nome || !cpf || !data_nascimento || !telefone || !email || !senha) {
         res.status(400).json({ mensagem: "Não foi possível criar uma nova conta, pois algum campo não foi preenchido." });
-    } else if (contas.find((conta) => conta.cpf === cpf)) {
-        return res.status(409).json({ mensagem: "Já existe uma conta com esse CPF." })
-    } else if (contas.find((conta) => conta.email === email)) {
-        return res.status(409).json({ mensagem: "Já existe uma conta com esse email." })
+    } else {
+        next();
+    }
+}
+
+function verificaDadosRepetidos(req, res, next) {
+    const { cpf, email } = req.body;
+    if ((contas.find((conta) => conta.usuario.cpf === cpf))) {
+        return res.status(409).json({ mensagem: "Já existe uma conta com esse CPF." });
+    } else if (contas.find((conta) => conta.usuario.email === email)) {
+        return res.status(409).json({ mensagem: "Já existe uma conta com esse email." });
     } else {
         next();
     }
@@ -22,7 +29,31 @@ function verificaListagemContas(req, res, next) {
     }
 }
 
+function verificaNumeroConta(req, res, next) {
+    const { numeroConta } = req.params;
+    const numeroExistente = contas.find((item) => item.numero === Number(numeroConta));
+    if (numeroExistente) {
+        next();
+    }
+}
+
+function verificaDadosRepetidosAtualizacao(req, res, next) {
+    const { cpf, email } = req.body;
+    const cpfRepetido = (contas.findIndex((conta) => conta.usuario.cpf === cpf));
+    const emailRepetido = (contas.findIndex((conta) => conta.usuario.email === email));
+    if ((cpfRepetido != (Number(req.params.numeroConta) - 1)) && (cpfRepetido != -1)) {
+        return res.status(409).json({ mensagem: "Já existe uma conta com esse CPF." });
+    } else if ((emailRepetido != (Number(req.params.numeroConta) - 1)) && (emailRepetido != -1)) {
+        return res.status(409).json({ mensagem: "Já existe uma conta com esse email." });
+    } else {
+        next();
+    }
+}
+
 module.exports = {
-    verificacaoNovaConta,
-    verificaListagemContas
+    verificaPreenchimentoCampos,
+    verificaListagemContas,
+    verificaNumeroConta,
+    verificaDadosRepetidos,
+    verificaDadosRepetidosAtualizacao
 }
