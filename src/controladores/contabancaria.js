8d56d63eb29b99bbcf5e711a7bb49f32dc37d96f
mirellaor <mirellaor>
2023-08-21 12:52:14 -0300

    função pra listar as contas criadas

--- src/controladores/contabancaria.js
+++ src/controladores/contabancaria.js
@@ -1,13 +1,21 @@
 const { banco, contas, saques, depositos, transferencias } = require('../bancodedados');
 let numero = 1;
+let saldo = 0;
 
 function criarConta(req, res) {
-    const novaConta = { numero, ...req.body };
+    let usuario = req.body;
+    const novaConta = { numero, saldo, usuario };
     contas.push(novaConta);
+
     res.status(201);
     numero++;
 }
 
+function listarContas(req, res) {
+    res.status(201).json(contas);
+}
+
 module.exports = {
-    criarConta
+    criarConta,
+    listarContas
 }
