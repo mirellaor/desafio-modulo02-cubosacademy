4255c5f1c309eda6f592b36b584be54654f67cf8
mirellaor <mirellaor>
2023-08-21 18:20:47 -0300

    rota para atualizar os dados daa conta

--- src/rotas.js
+++ src/rotas.js
@@ -4,7 +4,8 @@ const intermediadores = require('./intermediadores');
 
 const rotas = express();
 
-rotas.post('/contabancaria', intermediadores.verificacaoNovaConta, contabancaria.criarConta);
-rotas.get('/contabancaria', intermediadores.verificaListagemContas, contabancaria.listarContas);
+rotas.post('/contas', intermediadores.verificaPreenchimentoCampos, intermediadores.verificaDadosRepetidos, contabancaria.criarConta);
+rotas.get('/contas', intermediadores.verificaListagemContas, contabancaria.listarContas);
+rotas.put('/contas/:numeroConta/usuario', intermediadores.verificaPreenchimentoCampos, intermediadores.verificaNumeroConta, intermediadores.verificaDadosRepetidosAtualizacao, contabancaria.atualizarConta);
 
 module.exports = rotas;
