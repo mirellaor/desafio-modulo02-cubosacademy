e4bc65f43117e32c5f3ef296af37d4e02acbb124
mirellaor <mirellaor>
2023-08-22 17:35:37 -0300

    função de sacar

--- src/controladores/transacoes.js
+++ src/controladores/transacoes.js
@@ -10,9 +10,20 @@ function depositar(req, res) {
     res.status(201).json({ Mensagem: "Valor depositado." });
 }
 
+function sacar(req, res) {
+    const { numero_conta } = req.body;
+    const posicaoNumero_conta = (contas.findIndex((conta) => conta.numero === Number(numero_conta)));
+    contas[posicaoNumero_conta].saldo -= req.body.valor;
+
+    let dataNoSaque = { data: new Date().toLocaleString(), ...req.body }
+    saques.push(dataNoSaque);
+    res.status(201).json({ Mensagem: "Saque feito." });
+}
+
 
 
 
 module.exports = {
-    depositar
+    depositar,
+    sacar
 }
